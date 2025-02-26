5935694bafc6602a698050760b42365d758cc86b
HangmansMoose <trivial86@protonmail.com>
2025-02-27 06:09:24 +1100

    ruff config added to lspconfig

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -100,7 +100,6 @@ return {
         codelldb = {},
         -- gopls = {},
         -- pyright = {},
-        -- pylyzer = {}, -- note, needed to install node/npm for this plugin to successfully load
         rust_analyzer = {},
         omnisharp = {
           handlers = {
@@ -136,6 +135,34 @@ return {
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+		ruff = {
+    	    -- Notes on code actions: https://github.com/astral-sh/ruff-lsp/issues/119#issuecomment-1595628355
+    	    -- Get isort like behavior: https://github.com/astral-sh/ruff/issues/8926#issuecomment-1834048218
+    	    commands = {
+    	      RuffAutofix = {
+    	        function()
+    	          vim.lsp.buf.execute_command {
+    	            command = 'ruff.applyAutofix',
+    	            arguments = {
+    	              { uri = vim.uri_from_bufnr(0) },
+    	            },
+    	          }
+    	        end,
+    	        description = 'Ruff: Fix all auto-fixable problems',
+    	      },
+    	      RuffOrganizeImports = {
+    	        function()
+    	          vim.lsp.buf.execute_command {
+    	            command = 'ruff.applyOrganizeImports',
+    	            arguments = {
+    	              { uri = vim.uri_from_bufnr(0) },
+    	            },
+    	          }
+    	        end,
+    	        description = 'Ruff: Format imports',
+    	      },
+    	    },
+    	},
 
         lua_ls = {
           -- cmd = {...},
